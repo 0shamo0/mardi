950d10bf228c210dc4f647a894da0fa09b5de4ac
EC2 Default User <ec2-user@ip-172-31-26-187.us-east-2.compute.internal>
2020-03-04 22:52:00 +0000

    add validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,8 @@ class Task < ApplicationRecord
     belongs_to :project
     belongs_to :user
     belongs_to :step
+    
+    validates :task_name, presence: { message: "Nommer la tâche" }
+    validates :task_designation, presence: { message: "Description nécessaire." }
+    validates :date_butoire, presence: { message: "Date butoire obligatoire." }
 end
